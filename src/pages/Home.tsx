b9f4dcc7c0bc98f03f3bab89656629c82146517a
MackeyK24 <MackeyK24@gmail.com>
2026-08-04 11:35:02 -1000

    Home Page Update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -27,7 +27,7 @@ function Home() {
       </main>
 
       <footer className="splash-footer">
-        <small><a href="https://babylontoolkit.com" target="_blank">Babylon Toolkit Web Application Template</a></small>
+        <small><a href="https://babylontoolkit.com" target="_blank">Babylon Toolkit</a></small>
       </footer>
     </div>
   )
